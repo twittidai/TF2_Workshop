e9b48941b6111d32ca4eb95e30f1822a39dcf65c
Zenodia <Zenodia@users.noreply.github.com>
2020-02-24 15:34:10 +0100

    updated TF_MirroredStrategy scripts for profiling
    run tf2_MirroredStrategy.py for nsys profiling for 2_Nsight_run_TF2MirroredStrategy.sh
    run tf2_MirroredStrategy4line_profilder.py for run_line_profiler.sh

--- tf2_MirroredStrategy4line_profiler.py
+++ tf2_MirroredStrategy4line_profiler.py
@@ -14,20 +14,20 @@ from hvd.unet import custom_unet
 from loss_metrics import dice_loss, combined_dice_binary_loss , dice_coef
 import time
 batch_size=4
-
+@profile
 def one_hot(image, gt):
     gt_shape = tf.shape(gt)
     gt = tf.reshape(gt, gt_shape[:-1])
     gt = tf.one_hot(gt, 8)
     return image, gt
-
+@profile
 def decode_img(colored_image,ch=3):
     # convert the compressed string to a 3D uint8 tensor
     img = tf.image.decode_jpeg(colored_image, channels=ch)
     # Use `convert_image_dtype` to convert to floats in the [0,1] range.
 
     return img
-
+@profile
 def process_path(img_file_path):
     mask_file_path=tf.strings.regex_replace(img_file_path,'img','gt_cat')
     mask_file_path=tf.strings.regex_replace(mask_file_path,'_leftImg8bit.png', '_gtFine_color.png')# '_gtFine_labelIds.png')
@@ -35,7 +35,7 @@ def process_path(img_file_path):
     img_file = tf.io.read_file(img_file_path)
     mask_file = tf.io.read_file(mask_file_path)
     return img_file, mask_file
-
+@profile
 def decode(img_file, mask_file):
     #gt_file=tf.io.read_file(mask_file_path)
     im=decode_img(img_file,ch=3)
@@ -43,14 +43,14 @@ def decode(img_file, mask_file):
     print(im.shape, gt.shape)
     gt=tf.cast(gt,tf.uint8)
     return im,gt
-
+@profile
 def adapt_types_and_transpose(im, gt):
     im = tf.cast(im, tf.float32)
     #im = tf.transpose(im , (2,0,1)) # make sure that the input images are with the shape = (N_batches , Channels , Height, Width) for model to eat
     gt = tf.cast(gt, tf.uint8)
     return im, gt
 
-
+@profile
 def tf_data_pipeline_fn():
     img_files = tf.data.Dataset.list_files('/workspace/8data/img/*.png')
     dataset=img_files.map(lambda x: process_path(x)) 
@@ -63,7 +63,7 @@ def tf_data_pipeline_fn():
     
     dataset = dataset.prefetch(buffer_size=128)
     return dataset
-
+@profile
 def run_tf2strategy():
     AUTOTUNE = tf.data.experimental.AUTOTUNE
     NUM_WORKERS = 8 # number of gpus used for training
