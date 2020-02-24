d26b5106754f93eb84df2b7efa990eb835129d95
Zenodia <Zenodia@users.noreply.github.com>
2020-02-24 13:50:40 +0100

    Update 0_run_docker.sh

--- 0_run_docker.sh
+++ 0_run_docker.sh
@@ -1,1 +1,1 @@
- sudo docker run --runtime=nvidia -it --rm -p $:$1 --cap-add=SYS_ADMIN -v $2:/workspace nvcr.io/nvidia/tensorflow:19.12-tf2-py3  
+ sudo docker run --runtime=nvidia -it --rm -p $1:$1 --cap-add=SYS_ADMIN -v $2:/workspace nvcr.io/nvidia/tensorflow:19.12-tf2-py3  
