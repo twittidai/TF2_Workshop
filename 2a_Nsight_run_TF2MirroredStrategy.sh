baf920ad061d2238e0723da5a6f9f66278c2bb5b
Zenodia <Zenodia@users.noreply.github.com>
2020-02-29 10:16:58 +0100

    overwirte existing profile

--- 2a_Nsight_run_TF2MirroredStrategy.sh
+++ 2a_Nsight_run_TF2MirroredStrategy.sh
@@ -1,1 +1,1 @@
-nsys profile -y 30 -d 60 -w true -t "cudnn,cuda,osrt,nvtx" --trace-fork-before-exec=true -o profile_out$1 python tf2_MirroredStrategy.py 
+nsys profile -y 30 -d 60 -w true -t "cudnn,cuda,osrt,nvtx" --trace-fork-before-exec=true --force-overwrite true -o profile_out$1 python tf2_MirroredStrategy.py 
